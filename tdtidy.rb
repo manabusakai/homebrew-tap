273a399acd6fced674e637dbf2825ab8ac693b73
Manabu Sakai <rea.527@gmail.com>
2023-10-25 13:32:55 +0900

    feat: Bump tdtidy to 0.0.2

--- tdtidy.rb
+++ tdtidy.rb
@@ -1,7 +1,7 @@
 class Tdtidy < Formula
   desc "A command line tool for managing ECS task definitions."
   homepage "https://github.com/manabusakai/tdtidy"
-  version "0.0.1"
+  version "0.0.2"
 
   def install
     bin.install "tdtidy"
@@ -9,23 +9,23 @@ class Tdtidy < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/manabusakai/tdtidy/releases/download/v0.0.1/tdtidy_0.0.1_darwin_arm64.tar.gz"
-      sha256 "92fc24ebeb92cb919be96f608a81510c589270e12f8a1b742db413ba864228ba"
+      url "https://github.com/manabusakai/tdtidy/releases/download/v0.0.2/tdtidy_0.0.2_darwin_arm64.tar.gz"
+      sha256 "84e63b6c7a3fc3fc7f43f0f8816059b2266948d9cf9f092a939452f33deae07c"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/manabusakai/tdtidy/releases/download/v0.0.1/tdtidy_0.0.1_darwin_amd64.tar.gz"
-      sha256 "f19dea8635a5b5a90e48edb87c65fe58006cfce2780dba55db10d8f7d8a55a1e"
+      url "https://github.com/manabusakai/tdtidy/releases/download/v0.0.2/tdtidy_0.0.2_darwin_amd64.tar.gz"
+      sha256 "84046d2b0256e5c1601602200f15b8d8aa5a6dfd45351aeef92185240c7ef1ea"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/manabusakai/tdtidy/releases/download/v0.0.1/tdtidy_0.0.1_linux_arm64.tar.gz"
-      sha256 "6f8be8ecaf146599d4a17696116a97af6efef10a535e5ab3072782c62ad91a07"
+      url "https://github.com/manabusakai/tdtidy/releases/download/v0.0.2/tdtidy_0.0.2_linux_arm64.tar.gz"
+      sha256 "7a35e10511b1e06300d6c1b828a280dde7332970649fa5c0464025467fa8a6ce"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/manabusakai/tdtidy/releases/download/v0.0.1/tdtidy_0.0.1_linux_amd64.tar.gz"
-      sha256 "9e53df315283035daaf354a27a7f6db617d4ea5353a3a3c1054089f5f5766b34"
+      url "https://github.com/manabusakai/tdtidy/releases/download/v0.0.2/tdtidy_0.0.2_linux_amd64.tar.gz"
+      sha256 "a6809fe2a6eef7ffe51397311017c79dc278bd45bc6597816df248fbed9eefe4"
     end
   end
 end
