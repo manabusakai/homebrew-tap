bf39b8ab3ba266352a564710ff258461f12e7e14
Manabu Sakai <rea.527@gmail.com>
2023-11-06 22:26:24 +0900

    feat: Bump tdtidy to 0.0.3

--- tdtidy.rb
+++ tdtidy.rb
@@ -1,7 +1,8 @@
 class Tdtidy < Formula
   desc "A command line tool for managing ECS task definitions."
   homepage "https://github.com/manabusakai/tdtidy"
-  version "0.0.2"
+  version "0.0.3"
+  license "MIT"
 
   def install
     bin.install "tdtidy"
@@ -9,23 +10,23 @@ class Tdtidy < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/manabusakai/tdtidy/releases/download/v0.0.2/tdtidy_0.0.2_darwin_arm64.tar.gz"
-      sha256 "84e63b6c7a3fc3fc7f43f0f8816059b2266948d9cf9f092a939452f33deae07c"
+      url "https://github.com/manabusakai/tdtidy/releases/download/v0.0.3/tdtidy_0.0.3_darwin_arm64.tar.gz"
+      sha256 "2eecadc522cac0986945b43b86837912416edbd26e83b3aa7365dc029506e8f3"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/manabusakai/tdtidy/releases/download/v0.0.2/tdtidy_0.0.2_darwin_amd64.tar.gz"
-      sha256 "84046d2b0256e5c1601602200f15b8d8aa5a6dfd45351aeef92185240c7ef1ea"
+      url "https://github.com/manabusakai/tdtidy/releases/download/v0.0.3/tdtidy_0.0.3_darwin_amd64.tar.gz"
+      sha256 "1346827a1ba1f374f97a2e14513b85f667f8b63ed997f41bb862123325d4ab68"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/manabusakai/tdtidy/releases/download/v0.0.2/tdtidy_0.0.2_linux_arm64.tar.gz"
-      sha256 "7a35e10511b1e06300d6c1b828a280dde7332970649fa5c0464025467fa8a6ce"
+      url "https://github.com/manabusakai/tdtidy/releases/download/v0.0.3/tdtidy_0.0.3_linux_arm64.tar.gz"
+      sha256 "aadf427c30bda967eff44dc481e6e58f0651f1875e7d2752ec9d3118bbd59a60"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/manabusakai/tdtidy/releases/download/v0.0.2/tdtidy_0.0.2_linux_amd64.tar.gz"
-      sha256 "a6809fe2a6eef7ffe51397311017c79dc278bd45bc6597816df248fbed9eefe4"
+      url "https://github.com/manabusakai/tdtidy/releases/download/v0.0.3/tdtidy_0.0.3_linux_amd64.tar.gz"
+      sha256 "8e30debe837b3a4ff4ae5bcf480ea4c91481b953c0bb5d9d50af499d6d9947ab"
     end
   end
 end
