2e3f7eb737f33a5ccc737ecac073152d825aa3a1
Manabu Sakai <rea.527@gmail.com>
2024-04-07 21:46:00 +0900

    feat: Bump tdtidy to 0.0.4

--- tdtidy.rb
+++ tdtidy.rb
@@ -1,7 +1,7 @@
 class Tdtidy < Formula
   desc "A command line tool for managing ECS task definitions."
   homepage "https://github.com/manabusakai/tdtidy"
-  version "0.0.3"
+  version "0.0.4"
   license "MIT"
 
   def install
@@ -10,23 +10,23 @@ class Tdtidy < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/manabusakai/tdtidy/releases/download/v0.0.3/tdtidy_0.0.3_darwin_arm64.tar.gz"
-      sha256 "2eecadc522cac0986945b43b86837912416edbd26e83b3aa7365dc029506e8f3"
+      url "https://github.com/manabusakai/tdtidy/releases/download/v0.0.4/tdtidy_0.0.4_darwin_arm64.tar.gz"
+      sha256 "88d62cc91ebce30e21d8cd01c80df950cb3bcb4947a09b6543090116de2fc57f"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/manabusakai/tdtidy/releases/download/v0.0.3/tdtidy_0.0.3_darwin_amd64.tar.gz"
-      sha256 "1346827a1ba1f374f97a2e14513b85f667f8b63ed997f41bb862123325d4ab68"
+      url "https://github.com/manabusakai/tdtidy/releases/download/v0.0.4/tdtidy_0.0.4_darwin_amd64.tar.gz"
+      sha256 "952ca29fe955e4d197ad6fcc490bd2ceaac55c83c170373863719c3cd89def14"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/manabusakai/tdtidy/releases/download/v0.0.3/tdtidy_0.0.3_linux_arm64.tar.gz"
-      sha256 "aadf427c30bda967eff44dc481e6e58f0651f1875e7d2752ec9d3118bbd59a60"
+      url "https://github.com/manabusakai/tdtidy/releases/download/v0.0.4/tdtidy_0.0.4_linux_arm64.tar.gz"
+      sha256 "53f1312cbb27adeadd68c65830fe8f74fe8fe47b5d8817a65e8e445afd64dba5"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/manabusakai/tdtidy/releases/download/v0.0.3/tdtidy_0.0.3_linux_amd64.tar.gz"
-      sha256 "8e30debe837b3a4ff4ae5bcf480ea4c91481b953c0bb5d9d50af499d6d9947ab"
+      url "https://github.com/manabusakai/tdtidy/releases/download/v0.0.4/tdtidy_0.0.4_linux_amd64.tar.gz"
+      sha256 "2d842a884aecbd8a992e51511226f70ff264885f8518f8a3a31caf8a19945711"
     end
   end
 end
