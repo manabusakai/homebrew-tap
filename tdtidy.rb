4ec9765cfd5b52ac2b4805a6c89e539cabc5e01e
Manabu Sakai <rea.527@gmail.com>
2024-11-20 08:52:34 +0900

    feat: Bump tdtidy to 0.2.0

--- tdtidy.rb
+++ tdtidy.rb
@@ -1,7 +1,7 @@
 class Tdtidy < Formula
-  desc "A command line tool for managing ECS task definitions."
+  desc "A command line tool for managing ECS task definitions. `tdtidy` can deregister and delete old task definitions."
   homepage "https://github.com/manabusakai/tdtidy"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   def install
@@ -10,23 +10,23 @@ class Tdtidy < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/manabusakai/tdtidy/releases/download/v0.1.0/tdtidy_0.1.0_darwin_arm64.tar.gz"
-      sha256 "8feec8efe6451144097db92cf4c00b33965a607920590f4fcba37ee6f063c8b0"
+      url "https://github.com/manabusakai/tdtidy/releases/download/v0.2.0/tdtidy_0.2.0_darwin_arm64.tar.gz"
+      sha256 "6eb86d000b19c86df9b5ada2cef409571527ca16dba0fc119c3e1a932a39b382"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/manabusakai/tdtidy/releases/download/v0.1.0/tdtidy_0.1.0_darwin_amd64.tar.gz"
-      sha256 "677ef492f60cf625709e0474bf9efd5ff78baa55861d63bcca94c08d6bb76bc4"
+      url "https://github.com/manabusakai/tdtidy/releases/download/v0.2.0/tdtidy_0.2.0_darwin_amd64.tar.gz"
+      sha256 "8b6963ab964963861aa1994c9e59d172ce0d98f0a82083feecabd7e24192e7e2"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/manabusakai/tdtidy/releases/download/v0.1.0/tdtidy_0.1.0_linux_arm64.tar.gz"
-      sha256 "b346a0c98d85c6c365603b23fb4e23bfa99261308c36a79738f258f22d73f533"
+      url "https://github.com/manabusakai/tdtidy/releases/download/v0.2.0/tdtidy_0.2.0_linux_arm64.tar.gz"
+      sha256 "61bda120131f3e828472faf13f7af9002b4c63bf2b82881dbf9a7afe51e9572d"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/manabusakai/tdtidy/releases/download/v0.1.0/tdtidy_0.1.0_linux_amd64.tar.gz"
-      sha256 "102e4e44d08dd086f1ea0624dd7fa8a076c830d9591d510648c378d457639acd"
+      url "https://github.com/manabusakai/tdtidy/releases/download/v0.2.0/tdtidy_0.2.0_linux_amd64.tar.gz"
+      sha256 "2d5e14fed1861a280763658cb233781fc3730400e0fd0a90e35552c764b46e09"
     end
   end
 end
